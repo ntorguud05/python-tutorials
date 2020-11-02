0430a16cfbb735943bebbc6fc416473eddbc6c7b
Naran Torguud <ntorguud@gmail.com>
2020-11-02 06:48:48 +0000

    Create calc

---
+++ Calc.py
@@ -0,0 +1,15 @@
+x = int(input())
+y = int(input())
+
+op = int(input())
+if op == 1:
+    print('Amount is', x + y)
+
+if op == 2:
+    print('Difference is', x - y)
+
+if op == 3:
+    print(x * y)
+
+if op == 4:
+    print(x / y)
